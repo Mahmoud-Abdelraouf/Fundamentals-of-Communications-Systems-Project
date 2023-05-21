18e2d57724dc89bd8ebd17264f91d8d4f8a01354
Ahmed-Khaled-Abdelmaksod <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2023-05-22 00:30:11 +0300

    Add files via upload

---
+++ Part I/Receiver/detect_number_of_errors.m
@@ -0,0 +1,10 @@
+function number_of_errors = detect_number_of_errors(signal,sigma,samples_per_bit,bits)
+  number_of_errors = zeros(1,length(sigma));
+  for i=1:length(sigma)
+    noisy_signal = add_noise(signal,sigma(i),samples_per_bit);
+    recieved_bits = bipolar_rz_reciever(noisy_signal,10000,0,100);
+    ber_noise_bipolar_rz = calculate_ber(bits,recieved_bits);
+    number_of_errors(i) = ber_noise_bipolar_rz*10000;
+  endfor
+end
+
