ced9e7e979496ae3c0e48f80223578704b636690
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-05-23 14:51:34 +0300

    Some chages

--- Part I/Transmitter/part1_transmitter.m
+++ Part I/Transmitter/part1_transmitter.m
@@ -19,8 +19,29 @@ signal_4 = bipolar_rz(bits,1.2);
 %check manchester_coding
 signal_5 = manchester_coding(bits,1.2);
 
+%plot the spectral domain of Unipolar NRZ
+plot_spectral_domain(signal_1);
+%plot the spectral domain of polar NRZ
 plot_spectral_domain(signal_2);
-% Plot the eye diagram and set the plot limits
+%plot the spectral domain of Unipolar RZ
+plot_spectral_domain(signal_3);
+%plot the spectral domain of Bipolar RZ
+plot_spectral_domain(signal_4);
+%plot the spectral domain of Manchester Coding
+plot_spectral_domain(signal_5);
+
+%Plot the eye diagram of Unipolar NRZ
+eyediagram(signal_1, 300,1,1);
+xlim([-0.165, 0.5]);
+%Plot the eye diagram of polar NRZ
 eyediagram(signal_2, 300,1,1);
 xlim([-0.165, 0.5]);
+%Plot the eye diagram of Unipolar RZ
+eyediagram(signal_3, 300,1,1);
+%Plot the eye diagram of Bipolar RZ
+eyediagram(signal_4, 300,1,1);
+%Plot the eye diagram of Manchester Coding
+eyediagram(signal_5, 300,1,1);
+
+
 
